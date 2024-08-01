8d6143a3076b4d0e478694d7b44907a16dc8ede3
Kamil <kamilrahman32@gmail.com>
2024-08-01 10:32:42 +0530

    DB and Security Done

---
+++ BestTutor/src/main/java/com/example/demo/BestTutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestTutorApplication.class, args);
+	}
+
+}
